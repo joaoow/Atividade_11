c25d9275c3749f547ad0900db366cfdd6677d2ad
fsociety <87071803+joaolinux@users.noreply.github.com>
2021-07-22 10:41:20 -0300

    Digite um numero qualquer

---
+++ src/com/company/exercicio15.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class exercicio15 {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Digite um numero: ");
+        int num1 = scan.nextInt();
+        System.out.println("o numero digitado foi: " + num1);
+
+    }
+}
